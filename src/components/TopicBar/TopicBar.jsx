5a4409383aed5bdee429610d2eab20c425d3421b
Helen <huanghelen30@gmail.com>
2025-03-18 14:32:55 -0400

    create topicbar componenent

---
+++ src/components/TopicBar/TopicBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./TopicBar.scss";
+
+function TopicBar({topic}) {
+    return (
+        <div className="topicbar">
+            <p>Topic:{topic}</p>
+        </div>
+    );
+}
+
+export default TopicBar;
--- src/pages/TopicPage/TopicPage.jsx
+++ src/pages/TopicPage/TopicPage.jsx
@@ -1,9 +1,12 @@
+import CharacterCard from "/src/components/CharacterCard/CharacterCard.jsx";
+import TopicBar from "/src/components/TopicBar/TopicBar.jsx";
 import "./TopicPage.scss"
 
 function TopicPage() {
     
   return (
     <main className="topicpage">
+      <TopicBar />
     </main>
     
   )
